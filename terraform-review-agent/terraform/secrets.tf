74201caff311a471207208533a9c51bc2b08d685
Suraj Koditala <suraj.koditala@myridius.com>
2026-04-24 15:14:00 -0500

    gh workflow using iam role since access keys are giving error

--- terraform-review-agent/terraform/secrets.tf
+++ terraform-review-agent/terraform/secrets.tf
@@ -4,7 +4,7 @@ resource "aws_secretsmanager_secret" "gemini_api_key" {
 }
 
 resource "aws_secretsmanager_secret_version" "gemini_api_key_value" {
-  secret_id     = aws_secretsmanager_secret.gemini_api_key.id
+  secret_id = aws_secretsmanager_secret.gemini_api_key.id
   secret_string = jsonencode({
     GEMINI_API_KEY = var.gemini_api_key
   })
